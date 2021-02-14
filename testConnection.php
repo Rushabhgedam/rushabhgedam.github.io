08240558a9ef63c0a6fc2ff716e31a491e69d007
rushabhgedam <rushabhg358@gmail.com>
2021-02-14 12:46:31 +0530

    Motions added

--- testConnection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$link = mysqli_connect('localhost', 'root', 'Rushabh@8390');
-if (!$link) {
-die('Could not connect: ' . mysqli_error());
-}
-echo 'Connected successfully';
-mysqli_close($link);
-?>
